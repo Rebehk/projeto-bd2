0b16329afef5f4ec742a01e8ea72c2100beaf764
Ewerton <ewerton.maciel@cwi.com.br>
2022-08-09 15:56:00 -0300

    integrando front e backend

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors")
 const routes = require("./routes/routes");
 //para importar css
 const path = require("path");
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(routes);
 //app.use(express.static(path.join(__dirname, "public")));
 
--- pages/front.js
+++ pages/front.js
@@ -1,61 +1,176 @@
-const celulares = [{
-    fotoLink: "https://i.zst.com.br/thumbs/12/13/3e/-733839784.jpg",
-    modelo: "Redmi note 10",
-    fabricante: "Xiaomi",
-    preco: "R$ 1500,00",
-    processador: "Qualcom snapdragon",
-    memoriaInterna: "6GB",
-    memoriaRam: "128GB"
-},
-{
-    fotoLink: "https://i.zst.com.br/thumbs/49/13/35/-555742438.jpg",
-    modelo: "Redmi note 10",
-    fabricante: "Xiaomi",
-    preco: "R$ 1500,00",
-    processador: "Qualcom snapdragon",
-    memoriaInterna: "6GB",
-    memoriaRam: "128GB"
-}]
+const baseURL = 'http://localhost:3000'
+let dispositivos = []
 
-const div = document.querySelector(".container")
+//fetch methods
+function carregaDispositivos() {
+    fetch(`${baseURL}/inicio`)
+        .then(response => response.json())
+        .then(json => {
+            dispositivos = json
+            renderizaNaTela()
+        })
+        .catch(err => false)
+}
 
-celulares.forEach(cel => renderizaNaTela(cel))
+function handleAdd(dispositivo) {
+    fetch(`${baseURL}/add`, {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dispositivo)
+    }).then(response => {        
+        carregaDispositivos()
+    })
+        .catch(err => false)
+}
 
-function renderizaNaTela(celular) {
-    const {
-        fotoLink,
-        modelo,
-        fabricante,
-        preco,
-        processador,
-        memoriaInterna,
-        memoriaRam
-    } = celular
-    const html = `
-        <div class="card cards" style="width: 18rem;">
-            <img src=${fotoLink} class=" img-tam" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${modelo}</h5>
-                <p class="card-text">fabricante: ${fabricante}</p>
-                <p class="card-text">preço: ${preco}</p>
-                <p class="card-text">processador: ${processador}</p>
-                <p class="card-text">memoria interna: ${memoriaInterna}</p>
-                <p class="card-text">memoria ram: ${memoriaRam}</p>
-                <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    Opções
-                    </button>
-                    <ul class="dropdown-menu">
-                    <li><button class="btn-edit" data-bs-toggle="modal" data-bs-target="#modalForm">
-                    Editar
-                </button></li>
-                    <li><button class="btn-del" data-bs-toggle="modal" data-bs-target="#delete">
-                    Excluir
-                </button></li>
-                    </ul>
+function handleEdit(dispositivo) {
+    fetch(`${baseURL}/atualiza`, {
+        method: 'PUT',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dispositivo)
+    }).then(response => carregaDispositivos())
+        .catch(err => false)
+}
+
+function handleDelete(modelo) {
+    fetch(`${baseURL}/deleta/${modelo}`, {
+        method: 'DELETE',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(response => carregaDispositivos())
+        .catch(err => false)
+}
+
+//modal methods
+function renderizaNaTela() {
+    const div = document.querySelector(".container")
+    div.innerHTML = ''
+    if(dispositivos.length){
+        dispositivos.forEach(dispositivo => {
+            const {
+                fotoLink,
+                modelo,
+                fabricante,
+                preco,
+                processador,
+                memoriaInterna,
+                memoriaRam
+            } = dispositivo
+    
+            const html = `
+            <div class="card cards" style="width: 18rem;">
+                <img src=${fotoLink} class=" img-tam" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${modelo}</h5>
+                    <p class="card-text">fabricante: ${fabricante}</p>
+                    <p class="card-text">preço: ${preco}</p>
+                    <p class="card-text">processador: ${processador}</p>
+                    <p class="card-text">memoria interna: ${memoriaInterna}</p>
+                    <p class="card-text">memoria ram: ${memoriaRam}</p>
+                    <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        Opções
+                        </button>
+                        <ul class="dropdown-menu">
+                        <li><button class="btn-edit" onclick="handleSetModal('${modelo}', 'modalForm')" data-bs-toggle="modal" data-bs-target="#modalForm">
+                        Editar
+                    </button></li>
+                        <li><button onclick="handleSetModal('${modelo}', 'delete')" class="btn-del" data-bs-toggle="modal" data-bs-target="#delete">
+                        Excluir
+                    </button></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    `
-    div.innerHTML += html
+        `
+            div.innerHTML += html
+        })
+    }
+    else{
+        div.innerHTML = `
+            <div class="card cards" style="width: 18rem;">
+                <img src='./assets/image.jpg' class=" img-tam" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Nenhum dispositivo cadastrado</h5>
+                </div>
+            </div>
+        `
+    }
+    
+}
+
+function limparModalForm() {
+    document.querySelector('#fotolink').value = ''
+    document.querySelector('#fabricante').value = ''
+    document.querySelector('#modelo').value = ''
+    document.querySelector('#preco').value = ''
+    document.querySelector('#processador').value = ''
+    document.querySelector('#memoriaInterna').value = ''
+    document.querySelector('#memoriaRam').value = ''
+}
+
+function buscaDispositivoModalForm() {
+    return {
+        fotoLink: document.querySelector('#fotolink').value,
+        fabricante: document.querySelector('#fabricante').value,
+        modelo: document.querySelector('#modelo').value,
+        preco: document.querySelector('#preco').value,
+        processador: document.querySelector('#processador').value,
+        memoriaInterna: document.querySelector('#memoriaInterna').value,
+        memoriaRam: document.querySelector('#memoriaRam').value
+    }
+}
+
+function handleModal() {
+    const modal = document.querySelector('#modalForm')
+    const dispositivo = buscaDispositivoModalForm()
+
+    if (modal.getAttribute('modelo') && dispositivo) {
+        handleEdit(dispositivo)
+        modal.removeAttribute('modelo')
+    } else {
+        handleAdd(dispositivo)
+    }
+}
+
+function handleModalDelete() {
+    const modal = document.querySelector('#delete')
+    const modelo = modal.getAttribute('modelo')
+
+    if (modelo) {
+        handleDelete(modelo)
+    }
+
+    modal.removeAttribute('modelo')
+}
+
+function handleSetModal(modelo, idElem) {
+    if (modelo && idElem) {
+        document.querySelector(`#${idElem}`).setAttribute('modelo', modelo)
+        const dispositivo = dispositivos.find(dispositivo => dispositivo.modelo === modelo)
+        document.querySelector('#fotolink').value = dispositivo.fotoLink
+        document.querySelector('#fabricante').value = dispositivo.fabricante
+        document.querySelector('#modelo').value = dispositivo.modelo
+        document.querySelector('#preco').value = dispositivo.preco
+        document.querySelector('#processador').value = dispositivo.processador
+        document.querySelector('#memoriaInterna').value = dispositivo.memoriaInterna
+        document.querySelector('#memoriaRam').value = dispositivo.memoriaRam
+    }
+    else{
+        const modalFrom = document.querySelector(`#modalForm`)
+
+        if(modalFrom.getAttribute('modelo')){
+            modalFrom.removeAttribute('modelo')
+        }
+
+        limparModalForm() 
+    }
 }
